7d1077b341be8387519c4a8cbe9e539df6b87123
BugHunter-X <askzenai@gmail.com>
2024-12-11 21:50:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,10 @@
+function foo(a, b) {
+  a = a === undefined ? 0 : a;
+  b = b === undefined ? 0 : b;
+  return a + b;
+}
+
+console.log(foo(undefined, 1)); //1
+console.log(foo(null, 1)); //1
+console.log(foo(1, undefined)); //1
+console.log(foo(1, null)); //1
